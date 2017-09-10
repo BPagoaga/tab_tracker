9517a9f088e8377711b744d98d10f18095c590fc
BPagoaga <bernard.pagoaga@gmail.com>
2017-09-11 00:13:03 +0200

    [UPDATE] Adding sequelize, joi and vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-    <input type="email" name="email" placeholder="test@gmail.com" v-model="email">
-    <input type="password" name="password" placeholder="password" v-model="password">
+        <div class="pl-4 pr-4 pb-2 pl-2">
+          <input type="email" name="email" placeholder="test@gmail.com" v-model="email">
+          <input type="password" name="password" placeholder="password" v-model="password">
+          <div v-html="error" class="error"></div>
 
-    <button @click="register">Register</button>
-
-  </div>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -17,17 +25,22 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
 
-      console.log(response.data)
+        console.log(response.data)
+      } catch (err) {
+        this.error = err.response.data.error
+      }
     }
   }
 }
@@ -35,4 +48,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .error {
+    color: red;
+  }
 </style>
